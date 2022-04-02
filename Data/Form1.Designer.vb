d9970f1f61953bb16c5b49285954fca88e2124d1
RivalBaehaqi <rivalbaehaqi28@gmail.com>
2022-04-02 10:46:30 +0700

    first commit

---
+++ Data/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.npm = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.jr = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.pr = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.kls = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_search = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.npm, Me.nama, Me.jr, Me.pr, Me.kls})
+        Me.DataGridView1.Location = New System.Drawing.Point(182, 99)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(588, 304)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'npm
+        '
+        Me.npm.HeaderText = "Nomor pokok mahasiswa"
+        Me.npm.Name = "npm"
+        '
+        'nama
+        '
+        Me.nama.HeaderText = "Nama"
+        Me.nama.Name = "nama"
+        '
+        'jr
+        '
+        Me.jr.HeaderText = "jurusan"
+        Me.jr.Name = "jr"
+        '
+        'pr
+        '
+        Me.pr.HeaderText = "prodi"
+        Me.pr.Name = "pr"
+        '
+        'kls
+        '
+        Me.kls.HeaderText = "kelas"
+        Me.kls.Name = "kls"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(344, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "cari npm"
+        '
+        'txt_search
+        '
+        Me.txt_search.Location = New System.Drawing.Point(389, 34)
+        Me.txt_search.Name = "txt_search"
+        Me.txt_search.Size = New System.Drawing.Size(100, 20)
+        Me.txt_search.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(495, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 60)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(782, 355)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data mahasiswa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(912, 415)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt_search)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents npm As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents nama As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents jr As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents pr As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents kls As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_search As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ Data/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Web.Script.Serialization
+Public Class Form1
+
+    Private Sub getdata()
+        Try
+
+            Dim uriString As String = "http://localhost/CodeIgniter_API/index.php/api/GetData" & txt_search.Text.Trim
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "GET"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("npm").ToString, item("nama").ToString,
+                                       item("jurusan").ToString, item("prodi").ToString, item("kelas").ToString)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        getdata()
+    End Sub
+End Class
